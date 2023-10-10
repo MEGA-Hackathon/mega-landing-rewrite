1ffdd512eeda3d7f99bbafab736b55fcb693008c
Moyomade <adegbitemoyomade2004@gmail.com>
2023-10-10 13:19:13 +0100

    Created HomePage & SignupPage

--- v3-megalanding/src/App.vue
+++ v3-megalanding/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<HomePage />
+	<SignupPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue';
+import SignupPage from './components/SignupPage.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		HomePage,
+		SignupPage,
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	max-width: 100%;
+	overflow: hidden;
 }
 </style>
