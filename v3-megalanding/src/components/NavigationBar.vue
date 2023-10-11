248a1503e8c8233fdbb5c45d608d320bb4d61bc4
yeemans <arthur.roth.07@gmail.com>
2023-10-10 22:26:55 -0700

    added preliminary navbar

---
+++ v3-megalanding/src/components/NavigationBar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
+import { Button } from 'flowbite-vue'
+</script>
+<template>
+    <Navbar>
+      <template #logo>
+        <NavbarLogo link="/" alt="Flowbite logo" image-url="../assets/logo-dark.svg">
+        </NavbarLogo>
+      </template>
+      <template #default="{isShowMenu}">
+        <NavbarCollapse :isShowMenu="isShowMenu">
+          <NavbarLink isActive link="#">Home</NavbarLink>
+          <NavbarLink link="#">Events</NavbarLink>
+          <NavbarLink link="#">Impact</NavbarLink>
+          <Button color="dark">Sign in</Button>
+        </NavbarCollapse>
+      </template>
+    </Navbar>
+</template>
+<script>
+export default {
+    name: "NavigationBar"
+}
+</script>
+
+<style scoped>
+    .flex-container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .links {
+        margin: auto;
+    }
+
+    button {
+      color: white;
+      background: black;
+      font-size: 1.5em;
+      padding-top: 10px;
+      padding-bottom: 10px;
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+
+</style>
