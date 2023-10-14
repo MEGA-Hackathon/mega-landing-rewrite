e1170dada1bd1722eb38d9677e9e904177cd09da
yeemans <arthur.roth.07@gmail.com>
2023-10-14 10:54:13 -0700

    added logo and improved navbar

--- v3-megalanding/src/components/NavigationBar.vue
+++ v3-megalanding/src/components/NavigationBar.vue
@@ -1,52 +1,93 @@
-<script setup>
-  import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
-</script>
-
 <template>
-    <Navbar>
-      <template #logo>
-        <NavbarLogo link="/" alt="MEGA League" image-url="../assets/logo-dark.svg"> </NavbarLogo>
-      </template>
-      <template #default="{isShowMenu}">
-        <NavbarCollapse :isShowMenu="isShowMenu">
-          <NavbarLink is-active className="navLink">Home</NavbarLink>
-          <NavbarLink className="navLink">Events</NavbarLink>
-          <NavbarLink className="navLink">Impact</NavbarLink>
-        </NavbarCollapse>
-      </template>
-      <template #right-side>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
-      </template>
-    </Navbar>
+	<nav class="relative px-4 py-4 flex justify-between items-center bg-white">
+		<a class="text-3xl font-bold leading-none" href="#">
+			<img src="assets/logo-dark.svg">
+		</a>
+		<div class="lg:hidden">
+			<button class="navbar-burger flex items-center text-blue-600 p-3">
+				<svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<title>Mobile menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</button>
+		</div>
+		<ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+			<li><a class="text-xl text-gray-400 hover:text-gray-500" href="#">Home</a></li>
+			<li><a class="text-xl text-blue-600 font-bold" href="#">Events</a></li>
+			<li><a class="text-xl text-gray-400 hover:text-gray-500" href="#">Impact</a></li>
+		</ul>
+		<a class="hidden lg:inline-block py-2 px-6 bg-black text-xl text-white font-bold rounded-xl px-5 py-5" href="#">Sign up</a>
+	</nav>
+	<div class="navbar-menu relative z-50 hidden">
+		<div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+		<nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+			<div class="flex items-center mb-8">
+				<a class="mr-auto text-3xl font-bold leading-none" href="#">
+				</a>
+				<button class="navbar-close">
+				</button>
+			</div>
+			<div>
+				<ul>
+					<li class="mb-1">
+						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Home</a>
+					</li>
+					<li class="mb-1">
+						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Events</a>
+					</li>
+					<li class="mb-1">
+						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Impact</a>
+					</li>
+				</ul>
+			</div>
+			<div class="mt-auto">
+				<div class="pt-6">
+					<a class="block px-4 py-3 mb-2 leading-loose text-xl text-center text-white font-semibold bg-black rounded-xl" href="#">Sign Up</a>
+				</div>
+			</div>
+		</nav>
+	</div>
 </template>
+
 <script>
-export default {
-    name: "NavigationBar"
-}
+// Burger menus
+document.addEventListener('DOMContentLoaded', function() {
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (let i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (let i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
 </script>
-
-<style scoped>
-    .flex-container {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .links {
-        margin: auto;
-    }
-
-    button {
-      color: white;
-      background: black;
-      font-size: 1.5em;
-      padding-top: 10px;
-      padding-bottom: 10px;
-      padding-left: 5px;
-      padding-right: 5px;
-    }
-
-    .navLink {
-      font-size: 1.5em;
-    }
-
-</style>
